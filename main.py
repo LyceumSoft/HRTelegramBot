396866ba4a6c531aec95091027fb38620cfae649
ART0022VI <artem.jidov@gmail.com>
2023-09-29 20:06:48 +0700

    upd

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
 import json
+import logging
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
 AIAPI_KEY = ""
-BOT_TOKEN = ""
+TOKEN = ""
 with open('config.json', 'r', encoding='utf-8') as f:
     text = json.load(f)
     AIAPI_KEY = text['AI_API_KEY']
-    BOT_TOKEN = text['BOT_TOKEN']
+    TOKEN = text['BOT_TOKEN']
+
+def main() -> None:
+    updater = Updater(TOKEN)
+
+    dispatcher = updater.dispatcher
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            RESUME: [MessageHandler(Filters.text & ~Filters.command, process_resume)],
+            RESUME_TEXT: [MessageHandler(Filters.text & ~Filters.command, text)],
+        },
+        fallbacks=[CommandHandler('cancel', cancel)],
+    )
+
+    dispatcher.add_handler(conv_handler)
+
+    updater.start_polling()
+
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
