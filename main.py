816b2876d93ac54a93abde77f0bc734110c68409
Pelbmenb <65763960+ART0022VI@users.noreply.github.com>
2023-09-30 14:41:54 +0700

    upda

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import json
 import logging
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext
-AIAPI_KEY = ""
 TOKEN = ""
 with open('config.json', 'r', encoding='utf-8') as f:
     text = json.load(f)
-    AIAPI_KEY = text['AI_API_KEY']
     TOKEN = text['BOT_TOKEN']
+print(TOKEN)
+keyword_responses = {
+    "привет": "Привет!",
+    "компания": "Smart Consulting — это самостоятельная компания.",
+    "ответственность": "Ответственность — важная составляющая успеха для «Смартов».",
+}
