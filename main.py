a58dcf4df68693a9b5a428ce06bfbe3d83666fc7
Pelbmenb <65763960+ART0022VI@users.noreply.github.com>
2023-09-30 16:32:44 +0700

    первый интеллект

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
-import json
-import logging
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext
-TOKEN = ""
-with open('config.json', 'r', encoding='utf-8') as f:
-    text = json.load(f)
-    TOKEN = text['BOT_TOKEN']
-print(TOKEN)
-keyword_responses = {
-    "привет": "Привет!",
-    "компания": "Smart Consulting — это самостоятельная компания.",
-    "ответственность": "Ответственность — важная составляющая успеха для «Смартов».",
-}
+import nltk
+import string
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+with open("base.txt", "r", encoding="utf-8") as file:
+    text = file.read()
+
+sentences = nltk.sent_tokenize(text)
+
+def preprocess_text(text):
+    text = text.lower()
+    text = "".join([char for char in text if char not in string.punctuation])
+    return text
+
+vectorizer = TfidfVectorizer(tokenizer=nltk.word_tokenize, stop_words="english")
+tfidf_matrix = vectorizer.fit_transform([preprocess_text(sentence) for sentence in sentences])
+
+def get_response(question):
+    question = preprocess_text(question)
+    question_vec = vectorizer.transform([question])
+    similarities = cosine_similarity(question_vec, tfidf_matrix)
+    most_similar_sentence_index = np.argmax(similarities)
+    return sentences[most_similar_sentence_index]
+
+user_question = input()
+response = get_response(user_question)
+print(response)
