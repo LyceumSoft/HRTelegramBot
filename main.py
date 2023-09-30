d3076d356fbf66e031bd823565fcd49eb3353d71
Pelbmenb <65763960+ART0022VI@users.noreply.github.com>
2023-10-01 00:46:42 +0700

    нг-==

--- main.py
+++ main.py
@@ -12,26 +12,14 @@ while True:
     user_question = input("Задайте ваш вопрос (или 'exit' для выхода): ")
     if user_question.lower() == "exit":
         break
-
-    # Токенизация вопроса и текста
     inputs = tokenizer(user_question, text, return_tensors="pt", max_length=512, truncation=True)
-
-    # Получение оценок начала и конца ответа от модели
     start_scores, end_scores = model(**inputs)
-
-    # Нахождение начала и конца ответа на основе оценок
     answer_start = torch.argmax(start_scores).item()
     answer_end = torch.argmax(end_scores).item() + 1
-
-    # Получение фактически обрезанных токенов
-    input_ids = inputs["input_ids"][0].tolist()  # Преобразовать в список
+    input_ids = inputs["input_ids"][0].tolist()
     tokens = tokenizer.convert_ids_to_tokens(input_ids)
     truncated_tokens = tokens[answer_start:answer_end]
-
-    # Преобразование и вывод ответа
     answer_text = tokenizer.convert_tokens_to_string(truncated_tokens)
-
-    # Оценка уверенности модели в ответе
     confidence = (start_scores.max().item() + end_scores.max().item()) / 2
 
     print("Ответ:")
--- modeltutor.py
+++ modeltutor.py
@@ -1,57 +1,98 @@
 import torch
-from transformers import BertTokenizer, BertForQuestionAnswering
+import torch.nn as nn
+import torch.optim as optim
+import json
 
-# Загрузка предобученной модели BERT
-model_name = "bert-base-uncased"
-tokenizer = BertTokenizer.from_pretrained(model_name)
-model = BertForQuestionAnswering.from_pretrained(model_name)
+# Загрузка данных из JSON-файла
+with open('learnbase.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
 
-# Загрузка текста из файла base.md
-with open("base.md", "r", encoding="utf-8") as file:
-    text = file.read()
+questions = data["Q"]
+answers = data["A"]
 
-# Ваши вопросы и ответы
-questions = [
-    "Расскажи о компании",
-    "Кто мы?",
-    "Я только что устроился и это моя первая неделя",
-]
+# Создание словаря токенов и индекса
+vocab = set()
+for text in questions + answers:
+    vocab.update(text.split())
+print(len(vocab), vocab)
+word2index = {word: index for index, word in enumerate(vocab)}
+index2word = {index: word for word, index in word2index.items()}
 
-answers = [
-    "Когда-то давным-давно, году эдак в 2005-м, мы решили, что хотим что-то изменить в этой огромной и такой разнообразной стране. Хотим создавать улучшенную версию будущего благодаря IT-продуктам для государства и бизнеса. И… слава великой силе, Ктулху и Одину! У нас это получилось! И продолжает получаться! В те времена многие из нас еще не были знакомы и работали в разных компаниях. Но в глубине души нас объединяло желание сделать этот мир хоть чуточку, но удобнее.",
-    "МЫ —это сотрудники Smart Consulting! И если ты новичок в «Смартах»_(да-да, именно так называют нас наши клиенты)_, то добро пожаловать в компанию! Главное достояние Smart Consulting — это люди и их вклад в развитие нашей с тобой компании!",
-    "*Поздравляем! Ты прошел собеседование! Заполнил все необходимые бумаги, подписал кучу документов, каких-то согласий и стал частью**  **«**Смартов**»!** **Добро пожаловать в нашу команду!** _Если ты прошел собеседование лет так 10 назад, то твои действия__ —_ _встать, станцевать, улыбнуться, вспомнить первый день, офигеть от количества изменений и продолжить менять мир",
-]
+# Преобразование текста в индексы
+def text_to_indices(text, word2index):
+    return [word2index[word] for word in text.split()]
 
-while True:
-    user_question = input("Задайте ваш вопрос (или 'exit' для выхода): ")
-    if user_question.lower() == "exit":
-        break
+# Создание DataLoader и Batch
+from torch.utils.data import DataLoader, TensorDataset
 
-    # Токенизация вопроса и текста
-    inputs = tokenizer(user_question, text, return_tensors="pt")
+max_sequence_length = max(len(text.split()) for text in answers)
+question_indices = [text_to_indices(question, word2index) + [0] * (max_sequence_length - len(question.split())) for question in questions]
+answer_indices = [text_to_indices(answer, word2index) + [0] * (max_sequence_length - len(answer.split())) for answer in answers]
 
-    # Получение оценок начала и конца ответа от модели
-    start_scores, end_scores = model(**inputs)
+question_indices = torch.LongTensor(question_indices)
+answer_indices = torch.LongTensor(answer_indices)
 
-    # Нахождение начала и конца ответа на основе оценок
-    answer_start = torch.argmax(start_scores, dim=1).item()
-    answer_end = torch.argmax(end_scores, dim=1).item() + 1
+dataset = TensorDataset(question_indices, answer_indices)
+dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
 
-    # Получение фактически обрезанных токенов
-    input_ids = inputs["input_ids"][0].tolist()  # Преобразовать в список
-    tokens = tokenizer.convert_ids_to_tokens(input_ids)
-    truncated_tokens = tokens[answer_start:answer_end]
+# Создание модели seq2seq
+class Seq2Seq(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(Seq2Seq, self).__init__()
+        self.hidden_size = hidden_size
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.h = torch.zeros(1, 1, hidden_size)
 
-    # Преобразование и вывод ответа
-    answer_text = tokenizer.convert_tokens_to_string(truncated_tokens)
+    def forward(self, input, hidden):
+        embedded = self.embedding(input)
+        output, hidden = self.gru(embedded, hidden)
+        output = self.out(output.view(1, -1))
+        return output, hidden
 
-    # Оценка уверенности модели в ответе
-    confidence = (start_scores.max().item() + end_scores.max().item()) / 2
+# Инициализация модели и оптимизатора
+hidden_size = 256
+input_size = hidden_size
+output_size = len(vocab)
+model = Seq2Seq(input_size, hidden_size, output_size)
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+criterion = nn.CrossEntropyLoss()
 
-    print("Ответ:")
-    if confidence < 0.5:
-        print("Модель не уверена в ответе.")
-    else:
-        print(answer_text)
-    print(f"Уверенность: {confidence:.2f}")
+# Обучение модели
+def train(input_tensor, target_tensor, model, optimizer, criterion):
+    target_length = target_tensor.size(0)
+    optimizer.zero_grad()
+    loss = 0
+    hidden = torch.zeros(1, input_tensor.size(1), hidden_size)  # Исправьте размерность hidden
+    for i in range(input_tensor.size(0)):
+        output, hidden = model(input_tensor[i], hidden)
+        loss += criterion(output.squeeze(0), target_tensor[i])
+    loss.backward()
+    optimizer.step()
+    return loss.item() / target_length
+
+# Процесс обучения
+n_epochs = 100
+for epoch in range(n_epochs):
+    total_loss = 0
+    for batch in dataloader:
+        input_tensor, target_tensor = batch
+        loss = train(input_tensor, target_tensor, model, optimizer, criterion)
+        total_loss += loss
+    average_loss = total_loss / len(dataloader)
+    print(f"Epoch [{epoch + 1}/{n_epochs}], Loss: {average_loss:.4f}")
+
+# Генерация ответа на вопрос
+def generate_answer(question, model, word2index, index2word):
+    question_indices = torch.LongTensor(text_to_indices(question, word2index))
+    hidden = torch.zeros(1, 1, hidden_size)
+    output, _ = model(question_indices, hidden)
+    output_indices = output.squeeze(0).argmax(dim=1).tolist()
+    return indices_to_text(output_indices, index2word)
+
+# Тестирование модели
+test_question = input("Введите вопрос--")
+generated_answer = generate_answer(test_question, model, word2index, index2word)
+print(f"Question: {test_question}")
+print(f"Answer: {generated_answer}")
