c631aaaec538a2dc59488abe02258eba3ef90cdc
Pelbmenb <65763960+ART0022VI@users.noreply.github.com>
2023-09-30 16:53:37 +0700

    datasetupd

--- main.py
+++ main.py
@@ -4,26 +4,33 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-with open("base.txt", "r", encoding="utf-8") as file:
+# Загрузка предобученных данных
+with open("base.md", "r", encoding="utf-8") as file:
     text = file.read()
-
 sentences = nltk.sent_tokenize(text)
 
 def preprocess_text(text):
-    text = text.lower()
+    text = text.lower() 
     text = "".join([char for char in text if char not in string.punctuation])
     return text
 
+# Предобработка и TF-IDF векторизация текста
 vectorizer = TfidfVectorizer(tokenizer=nltk.word_tokenize, stop_words="english")
 tfidf_matrix = vectorizer.fit_transform([preprocess_text(sentence) for sentence in sentences])
 
+# Функция для получения ответа с более длинным текстом
 def get_response(question):
     question = preprocess_text(question)
     question_vec = vectorizer.transform([question])
     similarities = cosine_similarity(question_vec, tfidf_matrix)
-    most_similar_sentence_index = np.argmax(similarities)
-    return sentences[most_similar_sentence_index]
+    most_similar_sentence_indices = np.argsort(similarities[0])[-3:][::-1]  # Выберем три наиболее похожих предложения
+    response = "\n".join([sentences[idx] for idx in most_similar_sentence_indices])
+    return response
 
-user_question = input()
-response = get_response(user_question)
-print(response)
+while True:
+    user_question = input("Ваш вопрос (или 'exit' для выхода): ")
+    if user_question.lower() == "exit":
+        break
+    response = get_response(user_question)
+    print("Ответ:")
+    print(response)
