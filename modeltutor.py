9209dcc3558b69bd95430d3c881c105c5ae8c2d6
Pelbmenb <65763960+ART0022VI@users.noreply.github.com>
2023-10-01 09:33:35 +0700

    botwork

--- modeltutor.py
+++ modeltutor.py
@@ -1,98 +1,130 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
+import numpy as np
+import random
 import json
 
-# Загрузка данных из JSON-файла
-with open('learnbase.json', 'r', encoding='utf-8') as file:
-    data = json.load(file)
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
 
-questions = data["Q"]
-answers = data["A"]
+from nltk_utils import bag_of_words, tokenize, stem
+from model import NeuralNet
 
-# Создание словаря токенов и индекса
-vocab = set()
-for text in questions + answers:
-    vocab.update(text.split())
-print(len(vocab), vocab)
-word2index = {word: index for index, word in enumerate(vocab)}
-index2word = {index: word for word, index in word2index.items()}
+# Загрузка данных из файла intents.json
+with open('intents.json', 'r', encoding='utf-8') as f:
+    intents = json.load(f)
 
-# Преобразование текста в индексы
-def text_to_indices(text, word2index):
-    return [word2index[word] for word in text.split()]
+all_words = []
+tags = []
+xy = []
+# Проходим по каждому предложению в шаблонах намерений
+for intent in intents['intents']:
+    tag = intent['tag']
+    # Добавляем в список тегов
+    tags.append(tag)
+    for pattern in intent['patterns']:
+        # Токенизируем каждое слово в предложении
+        w = tokenize(pattern)
+        # Добавляем в список слов
+        all_words.extend(w)
+        # Добавляем в пару xy
+        xy.append((w, tag))
 
-# Создание DataLoader и Batch
-from torch.utils.data import DataLoader, TensorDataset
+# Проводим стемминг и приводим к нижнему регистру каждое слово
+ignore_words = ['?', '.', '!']
+all_words = [stem(w) for w in all_words if w not in ignore_words]
+# Удаляем дубликаты и сортируем
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
 
-max_sequence_length = max(len(text.split()) for text in answers)
-question_indices = [text_to_indices(question, word2index) + [0] * (max_sequence_length - len(question.split())) for question in questions]
-answer_indices = [text_to_indices(answer, word2index) + [0] * (max_sequence_length - len(answer.split())) for answer in answers]
+print(len(xy), "шаблонов")
+print(len(tags), "тегов:", tags)
+print(len(all_words), "уникальных слов:", all_words)
 
-question_indices = torch.LongTensor(question_indices)
-answer_indices = torch.LongTensor(answer_indices)
+# Создаем обучающие данные
+X_train = []
+y_train = []
+for (pattern_sentence, tag) in xy:
+    # X: мешок слов для каждого предложения
+    bag = bag_of_words(pattern_sentence, all_words)
+    X_train.append(bag)
+    # y: PyTorch CrossEntropyLoss требует только метки классов, не one-hot
+    label = tags.index(tag)
+    y_train.append(label)
 
-dataset = TensorDataset(question_indices, answer_indices)
-dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
+X_train = np.array(X_train)
+y_train = np.array(y_train)
 
-# Создание модели seq2seq
-class Seq2Seq(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(Seq2Seq, self).__init__()
-        self.hidden_size = hidden_size
-        self.embedding = nn.Embedding(input_size, hidden_size)
-        self.gru = nn.GRU(hidden_size, hidden_size)
-        self.out = nn.Linear(hidden_size, output_size)
-        self.h = torch.zeros(1, 1, hidden_size)
+# Гиперпараметры
+num_epochs = 1000
+batch_size = 8
+learning_rate = 0.001
+input_size = len(X_train[0])
+hidden_size = 8
+output_size = len(tags)
+print(input_size, output_size)
 
-    def forward(self, input, hidden):
-        embedded = self.embedding(input)
-        output, hidden = self.gru(embedded, hidden)
-        output = self.out(output.view(1, -1))
-        return output, hidden
+class ChatDataset(Dataset):
 
-# Инициализация модели и оптимизатора
-hidden_size = 256
-input_size = hidden_size
-output_size = len(vocab)
-model = Seq2Seq(input_size, hidden_size, output_size)
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = y_train
+
+    # Поддерживаем индексацию, чтобы можно было получить i-й образец dataset[i]
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    # Можем вызвать len(dataset), чтобы получить размер
+    def __len__(self):
+        return self.n_samples
+
+dataset = ChatDataset()
+train_loader = DataLoader(dataset=dataset,
+                          batch_size=batch_size,
+                          shuffle=True,
+                          num_workers=0)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model = NeuralNet(input_size, hidden_size, output_size).to(device)
+
+# Функция потерь и оптимизатор
 criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 # Обучение модели
-def train(input_tensor, target_tensor, model, optimizer, criterion):
-    target_length = target_tensor.size(0)
-    optimizer.zero_grad()
-    loss = 0
-    hidden = torch.zeros(1, input_tensor.size(1), hidden_size)  # Исправьте размерность hidden
-    for i in range(input_tensor.size(0)):
-        output, hidden = model(input_tensor[i], hidden)
-        loss += criterion(output.squeeze(0), target_tensor[i])
-    loss.backward()
-    optimizer.step()
-    return loss.item() / target_length
+for epoch in range(num_epochs):
+    for (words, labels) in train_loader:
+        words = words.to(device)
+        labels = labels.to(dtype=torch.long).to(device)
+        
+        # Прямой проход
+        outputs = model(words)
+        # если бы y было one-hot, мы должны были бы применить
+        # labels = torch.max(labels, 1)[1]
+        loss = criterion(outputs, labels)
+        
+        # Обратный проход и оптимизация
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+    if (epoch+1) % 100 == 0:
+        print (f'Эпоха [{epoch+1}/{num_epochs}], Потеря: {loss.item():.4f}')
 
-# Процесс обучения
-n_epochs = 100
-for epoch in range(n_epochs):
-    total_loss = 0
-    for batch in dataloader:
-        input_tensor, target_tensor = batch
-        loss = train(input_tensor, target_tensor, model, optimizer, criterion)
-        total_loss += loss
-    average_loss = total_loss / len(dataloader)
-    print(f"Epoch [{epoch + 1}/{n_epochs}], Loss: {average_loss:.4f}")
 
-# Генерация ответа на вопрос
-def generate_answer(question, model, word2index, index2word):
-    question_indices = torch.LongTensor(text_to_indices(question, word2index))
-    hidden = torch.zeros(1, 1, hidden_size)
-    output, _ = model(question_indices, hidden)
-    output_indices = output.squeeze(0).argmax(dim=1).tolist()
-    return indices_to_text(output_indices, index2word)
+print(f'Финальная потеря: {loss.item():.4f}')
 
-# Тестирование модели
-test_question = input("Введите вопрос--")
-generated_answer = generate_answer(test_question, model, word2index, index2word)
-print(f"Question: {test_question}")
-print(f"Answer: {generated_answer}")
+data = {
+"model_state": model.state_dict(),
+"input_size": input_size,
+"hidden_size": hidden_size,
+"output_size": output_size,
+"all_words": all_words,
+"tags": tags
+}
+
+FILE = "data.pth"
+torch.save(data, FILE)
+
+print(f'Обучение завершено. Файл сохранен как {FILE}')
